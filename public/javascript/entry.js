3e6e00041a6800588ae1bcb6af5e1134646c3a07
Mark Strelecky <streleck@gmail.com>
2017-09-27 16:09:08 -0500

    html entry, entryjs, login stuff

--- public/javascript/entry.js
+++ public/javascript/entry.js
@@ -1,4 +1,8 @@
 var user = sessionStorage.getItem('userName');
+if (user == null){
+	redirect("/loginauth");
+}
+console.log("user = " + user);
 
 //buttons for displaying each type of entry form
 $("#meal-entry").on("click", function(){
@@ -33,6 +37,7 @@ $("#meal-submit").on("click", function(){
 		carbs: $("#carbs").val(),
 		time: $("#meal-time").val()
 	}
+	console.log(dataSubmit);
 	$.post(currentURL + "/api/meal", dataSubmit,
 		function(data){
 			if (data.logged){
@@ -57,6 +62,7 @@ $("#sugar-submit").on("click", function(){
 		glucose: $("#glucose").val(),
 		time: $("#glucose-time").val()
 	}
+	console.log(dataSubmit);
 	$.post(currentURL + "/api/glucose", dataSubmit,
 		function(data){
 			if (data.logged){
@@ -80,19 +86,21 @@ $("#insulin-submit").on("click", function(){
 		type: $("#insulin-type").val(),
 		time: $("#insulin-time").val()
 	}
+	console.log(dataSubmit);
 	$.post(currentURL + "/api/insulin", dataSubmit,
 		function(data){
 			if (data.logged){
 				logSuccess();
 				$("#insulin-units").val("");
-				$("#insulin-time").val("");
+				$("#insulin-type").val("");
 				$("insulin-time").val("");
-				$("#sugar-form").css("display", "none");
+				$("#insulin-form").css("display", "none");
 			}
 			else{
 				logError();
 				$("#insulin-units").val("");
 				$("#insulin-time").val("");
+				$("#insulin-type").val("");
 			}
 	});
 });
@@ -102,14 +110,17 @@ $("#activity-submit").on("click", function(){
 		userName: user,
 		activity: $("#activity").val(),
 		mood: $("#mood").val(),
+		duration: $("#duration").val(),
 		time: $("#activity-time").val()
 	}
+	console.log(dataSubmit);
 	$.post(currentURL + "/api/activity", dataSubmit,
 		function(data){
 			if (data.logged){
 				logSuccess();
 				$("#activity").val("");
 				$("#mood").val("");
+				$("#duration").val("");
 				$("#activity-time").val("");
 				$("#activity-form").css("display", "none");
 			}
@@ -117,6 +128,7 @@ $("#activity-submit").on("click", function(){
 				logError();
 				$("#activity").val("");
 				$("#mood").val("");
+				$("#duration").val("");
 				$("#activity-time").val("");
 			}
 	});
