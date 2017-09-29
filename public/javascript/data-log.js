bb28bd4c93a0a176d4ff27acb0634255a9a05379
Mark Strelecky <streleck@gmail.com>
2017-09-29 10:23:24 -0500

    presentation mode

--- public/javascript/data-log.js
+++ public/javascript/data-log.js
@@ -5,14 +5,65 @@ var user = "jsmith";
 $("#query-submit").on("click", function(){
 	event.preventDefault();
 
-		console.log = $("#time-frame").val()
+		console.log($("#time-frame").val());
 	var querySubmit = {
 		userName: user,
 		dataType: $("#data-type").val(),
 		time: $("#time-frame").val()
 	}
 	$.post("/api/datalog", querySubmit,function(data){
-			
-		// console.log(data.key);
+		
+		if (querySubmit.dataType == "insulin"){
+		    var table = "";
+		    for(var i=0; i<data.key.length; i++){
+		        table += "<span>Units Administered: ";
+		        table += data.key[i].dataUnits;
+		        table += " | Type: ";
+		        table += data.key[i].dataKins;
+		        table += " | Time Taken(unix): ";
+		        table += data.key[i].dataTimeTaken
+		        table += "</span><br>"
+		    };
+		    $("#data-display").html(table);
+		}
+		else if (querySubmit.dataType == "activity "){
+		    var table = "";
+		    for(var i=0; i<data.key.length; i++){
+		        table += "<span>Level of Activity: ";
+		        table += data.key[i].dataLevel;
+		        table += " | Current Mood: ";
+		        table += data.key[i].dataMood;
+		        table += " | Activity Duration: ";
+		        table += data.key[i].dataTimeDuration;
+		            table += " | Time of Activity (unix): ";
+		        table += data.key[i].dataActivityTime
+		        table += "</span><br>"
+		    };
+		    $("#data-display").html(table);
+		}
+		else if (querySubmit.dataType == "bloodSugarM"){
+		    var table = "";
+		    for(var i=0; i<data.key.length; i++){
+		        table += "<span>mg/Dl: ";
+		        table += data.key[i].datamgDl;
+		        table += " | Time of Measurment: ";
+		        table += data.key[i].dataTimeOfM;
+		        table += "</span><br>"
+		    };
+		    $("#data-display").html(table);
+		}
+		else if (querySubmit.dataType == "meals"){
+		    var table = "";
+		    for(var i=0; i<data.key.length; i++){
+		        table += "<span>Meal: ";
+		        table += data.key[i].dataMeal;
+		        table += " | Carbs: ";
+		        table += data.key[i].dataCarbs;
+		        table += " | Time Eaten: ";
+		        table += data.key[i].dataTimeEaten;
+		        table += "</span><br>"
+		    };
+		    $("#data-display").html(table);
+		}
 	});
 });
